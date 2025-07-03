3b23abb81d0f0c4cd33c7e82d54655672f0273c9
Adan Cardoso <125130778+adancard@users.noreply.github.com>
2025-07-02 22:04:16 -0300

    Add files via upload

--- js/fichaDados.js
+++ js/fichaDados.js
@@ -9,17 +9,17 @@ const fichasDados = {
     imagem: 'https://adancard.github.io/Projetos_pessoais/img/Iselda.webp',
     descricao: 'Ela vende mapas e ferramentas de mapeamento, muitas vezes reclamando e desejando que Cornifer passasse mais tempo em casa e se preocupando com sua saúde e segurança.',
     item: {
-      CidadeLagrimas: { nome: 'Cidade das Lágrimas', valor: '120 Geo', img: 'img/Scroll_Map_Feather.webp' },
-      PicoCristal: { nome: 'Pico Cristal', valor: '150 Geo', img: 'img/Scroll_Map_Feather.webp' },
-      Ninho: { nome: 'Ninho Profundo', valor: '50 geos', img: 'img/Scroll_Map_Feather.webp' },
-      Canion: { nome: 'Cânion da Névoa', valor: '200 geos', img: 'img/Scroll_Map_Feather.webp' },
-      EncruzilhadaEsquecida: { nome: 'Encruzilhada Esquecida', valor: '40 geos', img: 'img/Scroll_Map_Feather.webp' },
-      Ermos: { nome: 'Ermos Fúngicos', valor: '100 geos', img: 'img/Scroll_Map_Feather.webp' },
-      CaminhoVerde: { nome: 'Caminho Verde', valor: '80 geos', img: 'img/Scroll_Map_Feather.webp' },
-      Penhascos: { nome: 'Penhascos Uivantes', valor: '100 geos', img: 'img/Scroll_Map_Feather.webp' },
-      Terra: { nome: 'Terra do Descanso', valor: '75 geos', img: 'img/Scroll_Map_Feather.webp' },
-      Hidrovia: { nome: 'Hidrovia Real', valor: '100 geos', img: 'img/Scroll_Map_Feather.webp' },
-      Jardins: { nome: 'Jardins da Rainha', valor: '200 geos', img: 'img/Scroll_Map_Feather.webp' },
+      CidadeLagrimas: { nome: 'Mapa Cidade das Lágrimas', valor: '120 Geo', img: 'img/Scroll_Map_Feather.webp' },
+      PicoCristal: { nome: 'Mapa Pico Cristal', valor: '150 Geo', img: 'img/Scroll_Map_Feather.webp' },
+      Ninho: { nome: 'Mapa Ninho Profundo', valor: '50 geos', img: 'img/Scroll_Map_Feather.webp' },
+      Canion: { nome: 'Mapa Cânion da Névoa', valor: '200 geos', img: 'img/Scroll_Map_Feather.webp' },
+      EncruzilhadaEsquecida: { nome: 'Mapa Encruzilhada Esquecida', valor: '40 geos', img: 'img/Scroll_Map_Feather.webp' },
+      Ermos: { nome: 'Mapa Ermos Fúngicos', valor: '100 geos', img: 'img/Scroll_Map_Feather.webp' },
+      CaminhoVerde: { nome: 'Mapa Caminho Verde', valor: '80 geos', img: 'img/Scroll_Map_Feather.webp' },
+      Penhascos: { nome: 'Mapa Penhascos Uivantes', valor: '100 geos', img: 'img/Scroll_Map_Feather.webp' },
+      Terra: { nome: 'Mapa Terra do Descanso', valor: '75 geos', img: 'img/Scroll_Map_Feather.webp' },
+      Hidrovia: { nome: 'Mapa Hidrovia Real', valor: '100 geos', img: 'img/Scroll_Map_Feather.webp' },
+      Jardins: { nome: 'Mapa Jardins da Rainha', valor: '200 geos', img: 'img/Scroll_Map_Feather.webp' },
       Bussola: { nome: 'Bússola Caprichosa', valor: '220 geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Wayward_Compass.webp' },
       Pena: { nome: 'Pena', valor: '120 geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Quill.webp' },
       PinoC: { nome: 'Pino de Comerciante', valor: '100 geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Map_Pin_Salubra.webp' },
@@ -37,9 +37,9 @@ const fichasDados = {
     imagem: 'https://adancard.github.io/Projetos_pessoais/img/Legeater.webp',
     descricao: 'Come Pernas é um comerciante eremita em Hollow Knight. Ele vende e repara Amuletos Frágeis em troca de Geo.',
     item: {
-      Coracao: { nome: 'Coração Frágil',valor: '380 geos/ 280 geos(se tiver o amuleto do defensor)/200 geos para reparo',img:'https://adancard.github.io/Projetos_pessoais/img/C10b_Fragile_Heart.webp'},
-      Ganancia: { nome: 'Ganância Frágil',valor: '250 geos/ 200 geos(se tiver o amuleto do defensor)/150 geos para reparo',img:'https://adancard.github.io/Projetos_pessoais/img/C11b_Fragile_Greed.webp'},
-      forca: { nome: 'Força Frágil',valor: '600 geos/ 480 geos(se tiver o amuleto do defensor)/350 geos para reparo',img:'https://adancard.github.io/Projetos_pessoais/img/C12b_Fragile_Strength.webp'}
+      Coracao: { nome: 'Coração Frágil', valor: '380 geos/ 280 geos(se tiver o amuleto do defensor)/200 geos para reparo', img: 'https://adancard.github.io/Projetos_pessoais/img/C10b_Fragile_Heart.webp' },
+      Ganancia: { nome: 'Ganância Frágil', valor: '250 geos/ 200 geos(se tiver o amuleto do defensor)/150 geos para reparo', img: 'https://adancard.github.io/Projetos_pessoais/img/C11b_Fragile_Greed.webp' },
+      forca: { nome: 'Força Frágil', valor: '600 geos/ 480 geos(se tiver o amuleto do defensor)/350 geos para reparo', img: 'https://adancard.github.io/Projetos_pessoais/img/C12b_Fragile_Strength.webp' }
     }
   },
   'pequeno-tolo': {
@@ -62,43 +62,43 @@ const fichasDados = {
     imagem: 'https://adancard.github.io/Projetos_pessoais/img/Lemm_Desk.webp',
     descricao: 'Colecionador de Relíquias Lemm é um Comerciante em Hollow Knight. Ele coleciona Relíquias e as compra por uma grande quantidade de Geo.',
     item: {
-        diario: {nome:'Diário de Viajante', valor: '220 Geos/Venda', img:'https://adancard.github.io/Projetos_pessoais/img/Wanderers_Journal.webp'},
-        selo: {nome:'Selo de Hallownest', valor: '450 Geos/Venda', img:'https://adancard.github.io/Projetos_pessoais/img/Hallownest_Seal.webp'},
-        idolo: {nome:'Ídolo do Rei', valor: '800 Geos/Venda', img:'https://adancard.github.io/Projetos_pessoais/img/Kings_Idol.webp'},
-        ovo: {nome:'Ovo Arcano', valor: '1200 Geos/Venda', img:'https://adancard.github.io/Projetos_pessoais/img/Arcane_Egg.webp'}
+      diario: { nome: 'Diário de Viajante', valor: '220 Geos/Venda', img: 'https://adancard.github.io/Projetos_pessoais/img/Wanderers_Journal.webp' },
+      selo: { nome: 'Selo de Hallownest', valor: '450 Geos/Venda', img: 'https://adancard.github.io/Projetos_pessoais/img/Hallownest_Seal.webp' },
+      idolo: { nome: 'Ídolo do Rei', valor: '800 Geos/Venda', img: 'https://adancard.github.io/Projetos_pessoais/img/Kings_Idol.webp' },
+      ovo: { nome: 'Ovo Arcano', valor: '1200 Geos/Venda', img: 'https://adancard.github.io/Projetos_pessoais/img/Arcane_Egg.webp' }
     }
   },
   'salubra': {
     nome: 'Salubra',
     imagem: 'https://adancard.github.io/Projetos_pessoais/img/Salubra_Circle.webp',
     descricao: 'A Salubra é uma vendedora de amuletos e encaixes de amuletos, após conseguir todos amuletos, ela ira te vender a “Benção de Salubra” por 800 Geo.',
-    item:{
-        Pedra: {nome:'Pedra do Xamã',valor:'220 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Shaman_Stone.webp'},
-        Corpo: {nome:'Corpo Firme',valor:'120 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Steady_Body.webp'},
-        Ferrao: {nome:'Ferrao Longo',valor:'300 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Longnail.webp'},
-        Foco: {nome:'Foco Rapido',valor:'800 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Quick_Focus.webp'},
-        Coracao: {nome:'Coração de Sangue',valor:'250 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Cora3Fo_de_Sangue_Vital.webp'}
+    item: {
+      Pedra: { nome: 'Pedra do Xamã', valor: '220 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Shaman_Stone.webp' },
+      Corpo: { nome: 'Corpo Firme', valor: '120 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Steady_Body.webp' },
+      Ferrao: { nome: 'Ferrao Longo', valor: '300 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Longnail.webp' },
+      Foco: { nome: 'Foco Rapido', valor: '800 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Quick_Focus.webp' },
+      Coracao: { nome: 'Coração de Sangue', valor: '250 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Cora3Fo_de_Sangue_Vital.webp' }
     }
   },
   'sly': {
     nome: 'Sly',
     imagem: 'https://adancard.github.io/Projetos_pessoais/img/Sly.webp',
     descricao: 'Inicialmente encontrado em um casebre na pequena vila após derrotar Mãe Mosca na Encruzilhada Esquecida, sucumbindo vagarosamente a Infecção. Ao abordá-lo seu transe é quebrado, então ele voltará à Dirtmouth para abrir sua loja.Quando é lhe dado a Chave do Comerciante, mais itens poderão ser acessados.',
-    item:{
-      chave: {nome:'Chave Simples',valor:'950 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/ChaveSimples.webp'},
-      ovo: {nome:'Ovo Rançoso',valor:'60 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/ovo.PNG'},
-      lanterna: {nome:'Lanterna de Lumélula',valor:'1800 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Lumafly_Lantern.webp'},
-      enxame: {nome:'Enxame de Colecionadores',valor:'300 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Gathering_Swarm.webp'},
-      carapaca: {nome:'	Carapaça Robusta',valor:'200 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Stalwart_Shell.webp'},
-      golpe: {nome:'Golpe pesado',valor:'350 Geos(requer chave do comerciante)',img:'https://adancard.github.io/Projetos_pessoais/img/Heavy_Blow.webp'},
-      chaveE: {nome:'Chave Elegante',valor:'800 Geos(requer chave do	Fragmento de Máscara',img:'https://adancard.github.io/Projetos_pessoais/img/Elegant_Key.webp'},
-      vida1: {nome:'	Fragmento de Máscara',valor:'150 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp'},
-      vida2: {nome:'	Fragmento de Máscara',valor:'500 Geos (Requer compra do Fragmento de Máscara anterior)',img:'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp'},
-      vida3: {nome:'	Fragmento de Máscara',valor:'800 Geos (Requer Chave do Comerciante e compra do Fragmento de Máscara anterior)',img:'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp'},
-      vida4: {nome:'	Fragmento de Máscara',valor:'1500 Geos (Requer Chave do Comerciante e compra do Fragmento de Máscara anterior)',img:'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp'},
-      fragmento1: {nome:'Fragmento de Receptáculo',valor:'550 Geos',img:'https://adancard.github.io/Projetos_pessoais/img/Vessel_Fragment.webp'},
-      fragmento2: {nome:'Fragmento de Receptáculo',valor:'900 Geos (Requer Chave do Comerciante e compra do Fragmento de Receptáculo anterior)',img:'https://adancard.github.io/Projetos_pessoais/img/Vessel_Fragment.webp'},
-      Mestre: {nome:'Mestre da Corrida',valor:'400 Geos (Requer chave do Comerciante)',img:'https://adancard.github.io/Projetos_pessoais/img/Sprintmaster.webp'}
+    item: {
+      chave: { nome: 'Chave Simples', valor: '950 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/ChaveSimples.webp' },
+      ovo: { nome: 'Ovo Rançoso', valor: '60 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/ovo.PNG' },
+      lanterna: { nome: 'Lanterna de Lumélula', valor: '1800 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Lumafly_Lantern.webp' },
+      enxame: { nome: 'Enxame de Colecionadores', valor: '300 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Gathering_Swarm.webp' },
+      carapaca: { nome: '	Carapaça Robusta', valor: '200 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Stalwart_Shell.webp' },
+      golpe: { nome: 'Golpe pesado', valor: '350 Geos(requer chave do comerciante)', img: 'https://adancard.github.io/Projetos_pessoais/img/Heavy_Blow.webp' },
+      chaveE: { nome: 'Chave Elegante', valor: '800 Geos(requer chave do	Fragmento de Máscara', img: 'https://adancard.github.io/Projetos_pessoais/img/Elegant_Key.webp' },
+      vida1: { nome: '	Fragmento de Máscara', valor: '150 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp' },
+      vida2: { nome: '	Fragmento de Máscara', valor: '500 Geos (Requer compra do Fragmento de Máscara anterior)', img: 'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp' },
+      vida3: { nome: '	Fragmento de Máscara', valor: '800 Geos (Requer Chave do Comerciante e compra do Fragmento de Máscara anterior)', img: 'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp' },
+      vida4: { nome: '	Fragmento de Máscara', valor: '1500 Geos (Requer Chave do Comerciante e compra do Fragmento de Máscara anterior)', img: 'https://adancard.github.io/Projetos_pessoais/img/Health_Piece.webp' },
+      fragmento1: { nome: 'Fragmento de Receptáculo', valor: '550 Geos', img: 'https://adancard.github.io/Projetos_pessoais/img/Vessel_Fragment.webp' },
+      fragmento2: { nome: 'Fragmento de Receptáculo', valor: '900 Geos (Requer Chave do Comerciante e compra do Fragmento de Receptáculo anterior)', img: 'https://adancard.github.io/Projetos_pessoais/img/Vessel_Fragment.webp' },
+      Mestre: { nome: 'Mestre da Corrida', valor: '400 Geos (Requer chave do Comerciante)', img: 'https://adancard.github.io/Projetos_pessoais/img/Sprintmaster.webp' }
     }
   },
   'jinn-alma-de-aço': {
@@ -108,12 +108,12 @@ const fichasDados = {
   },
   'o-último-besouro': {
     nome: 'O Último Besouro',
-    imagem:'https://adancard.github.io/Projetos_pessoais/img/Stag_Circle.webp',
+    imagem: 'https://adancard.github.io/Projetos_pessoais/img/Stag_Circle.webp',
     descricao: 'Ele pode ser encontrado em todas as regiões do jogo. Sua função é transportar o jogador entre as estações do Último Besouro.'
   },
   'tuk': {
     nome: 'Tuk',
-    imagem:'https://adancard.github.io/Projetos_pessoais/img/Tuk.webp',
+    imagem: 'https://adancard.github.io/Projetos_pessoais/img/Tuk.webp',
     descricao: 'Tuk é uma comerciante em Hollow Knight que pede Geo em troca de Ovo Rançoso.'
   }
 };
